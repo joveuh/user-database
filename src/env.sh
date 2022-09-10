5ccc96759cedbde891e42567eb564f1eca48a82c
Uzair <uhass924@gmail.com>
2022-09-09 21:06:29 -0400

    testing gitignore for .sh

--- src/env.sh
+++ src/env.sh
@@ -1,1 +1,158 @@
 #!/bin/bash
+
+echo test
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+
+echo test
+
