00976710e4dd91625cdd20e51dd91c18085da4c9
Uzair <uhass924@gmail.com>
2022-09-10 13:01:12 -0400

    Error Modals

--- src/components/users/UserList.js
+++ src/components/users/UserList.js
@@ -1,18 +1,22 @@
-import React, { useReducer } from "react";
+import React from "react";
 import Card from "../ui/Card";
 import styles from "./UserList.module.css";
+import ErrorModal from "../ui/ErrorModal";
 
 const UserList = (props) => {
   return (
-    <Card className={styles.users}>
-    <ul className={styles.users["ul"]}>
-      {props.users.map((user) => (
-        <li className={styles.users["li"]}  key={user.id}>
-          {user.name} ({user.age} years old)
-        </li>
-      ))}
-    </ul>
-    </Card>
+    <div>
+      <ErrorModal title="An error occured!" message="Something went wrong"/>
+      <Card className={styles.users}>
+        <ul className={styles.users["ul"]}>
+          {props.users.map((user) => (
+            <li className={styles.users["li"]} key={user.id}>
+              {user.name} ({user.age} years old)
+            </li>
+          ))}
+        </ul>
+      </Card>
+    </div>
   );
 };
 
