eba56f54d40a0e85dd33eeb08228f7db8574c7f4
Uzair <uhass924@gmail.com>
2022-09-09 21:02:42 -0400

    Coming together

---
+++ src/components/users/AddUser.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Card from "../ui/Card";
+import styles from './AddUser.module.css';
+import Button from "../ui/Button";
+
+const AddUser = props => {
+    const onSubmitHandler = event => {
+        event.preventDefault();
+  
+    }
+    return (
+        <Card className={styles.input}>
+            <form onSubmit = {onSubmitHandler}>
+                <label htmlFor="username">Username</label>
+                <input htmlFor="username" type="text"></input>
+                <label htmlFor="age">Age</label>
+                <input htmlFor="age" type="text"></input>
+                <Button type="submit">Add User</Button>
+            </form>
+        </Card>
+    )
+}
+
+export default AddUser;
